54684e06eeb525214f81136405c823a64ce9d51f
Muhammad Yousaf <yousufdevops09@gmail.com>
2026-04-09 12:57:38 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
-const app = express();   // Todo App Backend v2
+const app = express();   // Todo App Backend v3
 
 app.use(cors({
   origin: '*',
